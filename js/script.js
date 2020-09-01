8f6e004fb9a76a145c8be719d2024e588123d4a3
Alexander <soudryalex@gmail.com>
2020-09-01 16:31:01 -0400

    next Javascript Question

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
 var pokemonRepository = (function () 
 {
-	var pokemonList = $([]);
-		var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-			function loadList() 
-			{
-				// showLoadingMessage()
-			    $.ajax(apiUrl, { dataType: 'json'}).then(function (responseJSON) 
-			    {
-				      responseJSON.each(function (item) 
-				      {
-					       var pokemon = 
-					       {
-					         name: item.name,
-					         detailsUrl: item.url
-					       };
-				      })
-				 }).catch(function () 
-				    {
-				      console.error("error");
-				    })
-		  		return 
-		  		{
-		  			loadList: loadList
-				};
-            }
-})()
+    var pokemonList = [];
+    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+    function addListItem(pokemon) {
+        var button = $('<button>pokemon.name)</button>')
+        var listItem = $('<li>').append(button)
+        var poke = $('.pokemon-list').append(listItem)
+    }
+    function getAll() {
+        return pokemonList;
+    };
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+
+    };
+    function loadList() {
+        // showLoadingMessage()
+        return $.ajax(apiUrl, { dataType: 'json'}).then(function (response) { // I've returned the response on this line
+            // hideLoadingMessage()
+            $(response).each(function (item) {
+                var pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                }
+                add(pokemon)
+            }) 
+        }).catch(function () {
+            // hideLoadingMessage
+            console.error("error");
+        });
+    }
+
+    return {
+        loadList: loadList,
+        getAll: getAll,
+        addListItem: addListItem
+    };
+})();
+
 pokemonRepository.loadList().then(function() {
-	console.log("LoadList was read");
+    $(pokemonRepository.getAll).each(function(pokemon) {
+        pokemonRepository.addListItem(pokemon);
+        console.log("passing")
+    });
 });
