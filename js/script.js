fc8c672c54adb2b746e97f92e32f7c6a0e5b44e9
Alexander <soudryalex@gmail.com>
2020-09-24 22:21:10 -0400

    got button and modules working.

--- js/script.js
+++ js/script.js
@@ -3,31 +3,22 @@ var pokemonList = [];
 	
 	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-	// Modular Code Starts here
-      function showModal(pokemage, pokename, pokeheight) {
-    let modalBody = $(".modal-body")
-	let modalTitle = $(".modal-Title")
-	let modalHeader = $(".modal-header")
+	function loadList() {
+	    return $.ajax(apiUrl, {dataType: 'json'}).then(function (response) {
+	    	$.each(response.results, function ( index, pokemon) {
+	        addListItem(pokemon)
+	    })
+	  })
+	}
 
-	modalTitle.empty()
-	modalBody.empty()
-
-	let nameElement = $('"<h1>" + pokename </h1>');
-	let imageElement = $('<img class="modal_img" style="width:50%">').attr('"src", pokemage');
-	let heightElement = $('"<p>" + pokeheight + "</p>"');
-
-	modalTitle.append(nameElement);
-	modalBody.append(imageElement);
-	modalBody.append(heightElement);
-}
-	// Modular Code Ends Here
-      
-
-	function showDetails(pokemon) {
-	  	loadDetails(pokemon).then(function () {
-	  	showModal(pokemon.imageUrl, pokemon.name, pokemon.height)
-		  });
-		}
+	function addListItem(pokemon) {
+		var poke = $('.pokemon-list');
+		var listItem = $('<li>');
+		var button = $('<button class="btn btn-primary">').text(pokemon.name).attr('data-toggle', 'modal').attr('data-target', '#exampleModal');
+		listener(button, pokemon);
+		listItem.append(button);
+		poke.append(listItem);
+	};
 
 	function listener (button, pokemon) {
 		button.on('click', function () {
@@ -35,32 +26,48 @@ var pokemonList = [];
 		});
 	}
 
-	function addListItem(pokemon) {
-		var poke = $('.pokemon-list');
-		var listItem = $('<li>');
-		var button = $('<button class="btn btn-primary">').text(pokemon.name).attr('data-toggle="modal"', '').attr('data-target="#exampleModal"', '');
-		listener(button, pokemon);
-		listItem.append(button);
-		poke.append(listItem);
-	};
 
+	function showDetails(pokemon) {
+	  	loadDetails(pokemon).then(function () {
+	  		console.log(pokemon.name)
+	  		console.log(pokemon.height)
+	  	showModal(pokemon.imageUrl, pokemon.name, pokemon.height)
+		  });
+		}
 	
-
-	function loadList() {
-	    return $.ajax(apiUrl, {dataType: 'json'}).then(function (response) {
-	    	$.each(response.results, function ( index, pokemon) {
-	        addListItem(pokemon)
-	    })
-	  } )}
-	function loadDetails(item) {
+    function loadDetails(item) {
 	    var url = item.url;
 	    return $.ajax(url, {dataType: 'json'}).then(function (details) {
 	      // Now we add the details to the item
 	      item.imageUrl = details.sprites.front_default;
 	      item.height = details.height;
-	      item.types = details.types;
 	  })
 	  }
+
+	// Modular Code Starts here
+    function showModal(pokemage, pokename, pokeheight) {
+    console.log(pokename);
+    let modalBody = $(".modal-body")
+	let modalTitle = $(".modal-title")
+	let modalHeader = $(".modal-header")
+
+	modalTitle.empty()
+	modalBody.empty()
+
+	
+	let nameElement = $("<h1>" + pokename + "</h1>");
+	let imageElement = $('<img class="modal_img" style="width:50%">').attr("src", pokemage);
+	let heightElement = $("<p>" + pokeheight + "</p>");
+
+
+	modalTitle.append(nameElement);
+	modalBody.append(imageElement);
+	modalBody.append(heightElement);
+}
+	// Modular Code Ends Here
+   
+	
+	//   }
 	  // The properties the repository can return
 	  return {
 	    loadList: loadList,
